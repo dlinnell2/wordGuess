f1f0a971084e3f7177129e68be7f9e3020206120
Donald Linnell <dlinnell@donalds-air.attlocal.net>
2018-04-09 18:02:56 -0500

    Full functionality

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ var Word = require('./Word.js');
 var inquirer = require('inquirer');
 
 var chosenWord;
+var checkWord;
 var guessCount;
 var guessed = [];
 var userGuess;
 
 var validLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-var batman = new Word('batman');
 
 var wordChoices = ['Batman', 'Robin', 'Batgirl', 'Nightwing', 'Red Hood', 'Oracle', 'Bane', 'Riddler', 'Joker', 'Harley Quinn', 'Poison Ivy', 'Penquin', 'Catwoman', 'Hush', 'Alfred', 'Arkham', 'Clayface', 'Killer Croc', 'Gordon', 'Mad Hatter', 'Mr Freeze', 'Two Face', 'Scarecrow', 'Bullock', 'Bruce', 'Wayne', 'Grayson'];
 
@@ -21,7 +20,7 @@ function wordIndex(x) {
 // Selects the computers answer using previous function and array
 
 function chooseWord() {
-    return wordChoices[wordIndex(wordChoices.length)].toLowerCase();
+    return wordChoices[wordIndex(wordChoices.length)];
 }
 
 function start() {
@@ -45,18 +44,20 @@ function start() {
 };
 
 function createDisplay() {
-    console.log(chosenWord.displayArr.join(' '));
+    console.log(checkWord.displayArr.join(' '));
     console.log(`Incorrect Guesses Remaining : ${guessCount}`);
 };
 
 function gameSet() {
-    chosenWord = new Word(chooseWord());
-    chosenWord.buildArray();
+
+    chosenWord = chooseWord();
+    checkWord = new Word(chosenWord.toLowerCase());
+    checkWord.buildArray();
 
     guessCount = 5;
+    guessed = [];
 
     createDisplay();
-
     gameRun();
 
 };
@@ -70,29 +71,33 @@ function checkGuess(rightOrWrong) {
 
 function gameRun() {
 
-    if ((guessCount > 0) && (chosenWord.displayArr.indexOf('_') > -1)) {
+    if ((guessCount > 0) && (checkWord.displayArr.indexOf('_') > -1)) {
 
         inquirer.prompt([
+
             {
                 message: 'Please enter a letter to guess!',
                 name: 'guess',
                 validate: function (value) {
+
                     if (validLetters.indexOf(value.toLowerCase()) > -1) {
                         return true;
                     } else if (value.length > 1) {
                         console.log('\n Please enter a single letter to continue')
                         return false;
                     }
+
                     console.log('\n Please enter a valid letter to continue')
                     return false;
                 }
             }
+
         ]).then(function (answer) {
 
             userGuess = answer.guess;
 
-            for (var item of chosenWord.letters) {
-                console.log(item);
+            for (var item of checkWord.letters) {
+    
                 if ((item.letter === userGuess) && (item.guess) || (guessed.indexOf(userGuess) > -1)) {
                     console.log(`You've already guessed that letter!`);
 
@@ -103,13 +108,13 @@ function gameRun() {
                 } else if ((item.letter === userGuess) && (!item.guess)) {
                     console.log(`Correct Guess!`);
 
-                    checkGuess(chosenWord.isRight(userGuess));
+                    checkGuess(checkWord.isRight(userGuess));
 
                     return;
 
-                }
+                };
 
-            }
+            };
 
             console.log('Incorrect Guess!');
             
@@ -119,10 +124,38 @@ function gameRun() {
 
         });
 
-    } else {
-        console.log(`You've Won!`);
-        process.exit();
+    } else if (guessCount === 0) {
+        console.log(`Sorry! The word was ${chosenWord}`);
+        beginAgain();
+
+    } else if (checkWord.displayArr.indexOf('_') === -1) {
+        console.log(`Congrats! You've correctly guessed the word ${chosenWord}!`);
+        beginAgain();
     }
+
+};
+
+function beginAgain(){
+
+    inquirer.prompt([
+
+        {
+        type: 'confirm',
+        name: 'beginAgain',
+        message: 'Would you like to play again?'
+        }
+
+    ]).then(function(answer){
+        if (answer.beginAgain){
+            console.log('Let\'s begin!')
+            gameSet();
+        } else {
+            console.log('Thanks for playing!');
+            process.exit();
+        }
+
+    });
+
 };
 
 start();
